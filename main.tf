e2e43e2047f59a63d3c62fe91687e593b931d7a1
emilymacpro <attsun1031@gmail.com>
2020-12-03 01:40:15 +0900

    make New branch Subnet

--- main.tf
+++ main.tf
@@ -16,32 +16,13 @@ resource "azurerm_policy_assignment" "Allowed_locations" {
   description          = "This policy enables you to restrict the locations your organization can specify when deploying resources. Use to enforce your geo-compliance requirements. Excludes resource groups, Microsoft.AzureActiveDirectory/b2cDirectories, and resources that use the 'global' region."
   display_name         = "Allowed locations"
   identity {
-      type = "SystemAssigned"
+    type = "SystemAssigned"
   }
-  location = "eastus"
+  location   = "eastus"
   parameters = <<PARAMETERS
     {
       "listOfAllowedLocations": {
         "value": [ "East US","East US 2" ]
-      }
-    }
-  PARAMETERS
-}
-
-resource "azurerm_policy_assignment" "inherit_tag_from_rg" {
-  name                 = "Inherit-tag-from-resourcegroup"
-  scope                = azurerm_resource_group.test01.id
-  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/cd3aa116-8754-49c9-a813-ad46512ece54"
-  description          = "Adds or replaces the specified tag and value from the parent resource group when any resource is created or updated. Existing resources can be remediated by triggering a remediation task."
-  display_name         = "Inherit a tag from the resource group"
-  identity {
-      type = "SystemAssigned"
-  }
-  location = "eastus"
-  parameters = <<PARAMETERS
-    {
-      "tagName": {
-        "value": "Owner"
       }
     }
   PARAMETERS
@@ -53,20 +34,9 @@ resource "azurerm_virtual_network" "test01" {
   resource_group_name = azurerm_resource_group.test01.name
   location            = azurerm_resource_group.test01.location
   address_space       = ["10.0.0.0/16"]
+  tags = {
+    "Owner" = "koizumi",
+    "Env"   = "test01"
+  }
 }
 
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "test02" {
-  name                = "test02"
-  resource_group_name = azurerm_resource_group.test01.name
-  location            = "East US 2"
-  address_space       = ["10.0.0.0/16"]
-}
-
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "test03" {
-  name                = "test03"
-  resource_group_name = azurerm_resource_group.test01.name
-  location            = "West US 2"
-  address_space       = ["10.0.0.0/16"]
-}
